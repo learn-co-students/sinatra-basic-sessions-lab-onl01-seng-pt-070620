183b206bff939c8e2f48d73b098c702c1fbd8dae
kinnari <kinnari1022@yahoo.com>
2020-10-02 00:21:39 -0400

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,18 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+    get '/' do
+    
+        erb :index
+    end
+
+    post '/checkout' do
+        session[:item] = "socks"
+        @session = session
+    
+        erb :checkout
+    end
+   
+
 end
